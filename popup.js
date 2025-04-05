58dbfda8fdcbe217fb091b2440d0ba5f5bf2e469
Speeqw <117276166+Speeqw@users.noreply.github.com>
2025-04-05 15:45:29 +0800

    Update popup.js

--- popup.js
+++ popup.js
@@ -251,7 +251,7 @@ const contentData = {
     `,
     about: `
         <h2>About the Project</h2>
-        <p>The <a href="#" class="link">Singapore Building Carbon Map</a> is an interactive platform developed by <a href="https://www.citysyntax.io/" class="link">City Syntax Lab</a> at the <a href="https://nus.edu.sg/" class="link">National University of Singapore (NUS)</a>. It visualizes and analyzes the carbon emissions of buildings across Singapore, covering both operational and embodied carbon footprints. By integrating geographic data with building performance insights, the platform empowers policymakers, industry professionals, and the public to identify opportunities for carbon reduction. This tool supports Singapore's <a href="https://www.greenplan.gov.sg/" class="link">Green Plan 2030</a> by promoting energy-efficient designs, sustainable materials, and low-carbon construction practices, driving progress toward a more sustainable built environment.
+        <p>The <a href="#" class="link">Buildings.sg</a> is an interactive platform developed by <a href="https://www.citysyntax.io/" class="link">City Syntax Lab</a> at the <a href="https://nus.edu.sg/" class="link">National University of Singapore (NUS)</a>. It visualizes and analyzes the carbon emissions of buildings across Singapore, covering both operational and embodied carbon footprints. By integrating geographic data with building performance insights, the platform empowers policymakers, industry professionals, and the public to identify opportunities for carbon reduction. This tool supports Singapore's <a href="https://www.greenplan.gov.sg/" class="link">Green Plan 2030</a> by promoting energy-efficient designs, sustainable materials, and low-carbon construction practices, driving progress toward a more sustainable built environment.
         <br><br>The Map leverages the <a href="https://www.ubem.io/" class="link">Urban Building Energy Model (UBEM)</a> framework to assess and visualize the carbon emissions of buildings across Singapore. UBEM provides a simulation-based approach to model energy usage, carbon footprints, and environmental impacts of buildings on a city-wide scale, providing valuable insights for reducing emissions and improving energy efficiency in urban planning.</p>
         <div style="text-align: center; margin-top: 10px;">
             <a href="https://www.citysyntax.io/">
@@ -271,7 +271,7 @@ const contentData = {
         <br>
         
         <h2>Disclaimer</h2>
-        <p><strong>Buildings.sg</strong> presents carbon emission data for informational purposes only. This information should not be used as the basis for legal, financial, or regulatory decisions. Despite our commitment to accuracy, we make no warranties regarding the precision, completeness, or reliability of this data. 
+        <p>Buildings.sg presents carbon emission data for informational purposes only. This information should not be used as the basis for legal, financial, or regulatory decisions. Despite our commitment to accuracy, we make no warranties regarding the precision, completeness, or reliability of this data. 
         Users should independently verify all information through official sources before taking any action based on this tool. The developers, contributors, and affiliated organizations expressly disclaim all liability for any consequences arising from the use of this tool.</p>
         `
 };
